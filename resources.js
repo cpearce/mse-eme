f808e1ed0de16ec34f7240affabfcbeb693b9300
Chris Pearce <cpearce@mozilla.com>
2015-11-13 15:21:46 +1300

    Update resources.js to include new bento-4 encoded files.

--- resources.js
+++ resources.js
@@ -1,3 +1,4 @@
+/*
 var audioFragments = [
   "media/sintel_trailer-720p-frag-cenc_audioinit.mp4",
   "media/sintel_trailer-720p-frag-cenc_audio1.m4s",
@@ -22,5 +23,50 @@ var videoFragments = [
   "media/sintel_trailer-720p-frag-cenc_video6.m4s",
   "media/sintel_trailer-720p-frag-cenc_video7.m4s",
   "media/sintel_trailer-720p-frag-cenc_video8.m4s",
+];
+*/
+
+
+var audioFragments = [
+  "media/video-00-init.mp4",
+  "media/video-01.mp4",
+  "media/video-02.mp4",
+  "media/video-03.mp4",
+  "media/video-04.mp4",
+  "media/video-05.mp4",
+  "media/video-06.mp4",
+  "media/video-07.mp4",
+  "media/video-08.mp4",
+  "media/video-09.mp4",
+];
+
+var videoFragments = [
+  "media/audio-00-init.mp4",
+  "media/audio-01.mp4",
+  "media/audio-02.mp4",
+  "media/audio-03.mp4",
+  "media/audio-04.mp4",
+  "media/audio-05.mp4",
+  "media/audio-06.mp4",
+  "media/audio-07.mp4",
+  "media/audio-08.mp4",
+  "media/audio-09.mp4",
+  "media/audio-10.mp4",
+  "media/audio-11.mp4",
+  "media/audio-12.mp4",
+  "media/audio-13.mp4",
+  "media/audio-14.mp4",
+  "media/audio-15.mp4",
+  "media/audio-16.mp4",
+  "media/audio-17.mp4",
+  "media/audio-18.mp4",
+  "media/audio-19.mp4",
+  "media/audio-20.mp4",
+  "media/audio-21.mp4",
+  "media/audio-22.mp4",
+  "media/audio-23.mp4",
+  "media/audio-24.mp4",
+  "media/audio-25.mp4",
+  "media/audio-26.mp4",
 ];
 
