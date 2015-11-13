480f3f3ec7dcd26bedb3623c20c79b884b22993e
Chris Pearce <cpearce@mozilla.com>
2015-11-13 21:54:31 +1300

    Use Bento4's mp4-dash.py to generate the fragements, so that they're compatible with Chrome. Firefox can handle the MP4Box fragments, but Chrome cannot.

--- media/bento-encrypt.sh
+++ media/bento-encrypt.sh
@@ -10,7 +10,19 @@ mp4encrypt --method MPEG-CENC --key 1:7f412f0575f44f718259beef56ec7771:0a8d9e585
 mp4encrypt --method MPEG-CENC --key 2:624db3d757bb496fb93e51f341d11716:bf07e864e27643a0 --property 2:KID:7eaa636ee7d142fd945d1f764877d8db --global-option mpeg-cenc.eme-pssh:true sintel_trailer-720p-audio-fragmented.mp4 sintel_trailer-720p-audio-fragmented-cenc.mp4
 
 # Split fragmented files into chunks.
-mp4split.exe --init-segment audio-00-init.mp4 --media-segment audio-%02llu.mp4 --pattern-parameters N sintel_trailer-720p-audio-fragmented-cenc.mp4
+# Note: This produces files that work in Firefox but not Chrome, so don't use it.
+#mp4split.exe --start-number 1 --media-segment audio-%02llu.mp4 --init-segment audio-00-init.mp4 --track-id 2 --pattern-parameters N sintel_trailer-720p-audio-fragmented-cenc.mp4
+#mp4split.exe --start-number 1 --media-segment video-%02llu.mp4 --init-segment video-00-init.mp4 --track-id 1 --pattern-parameters N sintel_trailer-720p-video-fragmented-cenc.mp4
 
-mp4split.exe --init-segment video-00-init.mp4 --media-segment video-%02llu.mp4 --pattern-parameters N sintel_trailer-720p-video-fragmented-cenc.mp4
- 
+# use mp4-dash.py from Bento; it outputs fragments that are decodable by Chrome.
+python ~/Downloads/Bento4-SDK-1-4-3-603.x86-microsoft-win32-vs2010/Bento4-SDK-1-4-3-603.x86-microsoft-win32-vs2010/utils/mp4-dash.py -f -o fragments sintel_trailer-720p-audio-fragmented-cenc.mp4
+python ~/Downloads/Bento4-SDK-1-4-3-603.x86-microsoft-win32-vs2010/Bento4-SDK-1-4-3-603.x86-microsoft-win32-vs2010/utils/mp4-dash.py -f -o fragments sintel_trailer-720p-video-fragmented-cenc.mp4
+
+rm -rf audio
+rm -rf video
+mv fragments/audio/und audio
+mv fragments/video/1 video
+rm -rf fragments/
+
+for f in audio/*.m4f; do mv "$f" "`echo $f | sed s/m4f/mp4/`"; done
+for f in video/*.m4f; do mv "$f" "`echo $f | sed s/m4f/mp4/`"; done
